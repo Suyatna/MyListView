f2f4aba7641339f7be35041e2dae7f8c11f6f0a1
suyatna <antasuy@gmail.com>
2019-06-30 17:03:50 +0700

    adding project

---
+++ app/src/main/java/com/zuyatna/mylistview/Hero.java
@@ -0,0 +1,32 @@
+package com.zuyatna.mylistview;
+
+public class Hero {
+
+    private int photo;
+    private String Name;
+    private String description;
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
